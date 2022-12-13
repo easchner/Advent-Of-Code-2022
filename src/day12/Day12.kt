faae640b5b4b1ba4f562f676e9e92b68bc531626
easchner <eric_aschner@hotmail.com>
2022-12-12 22:55:29 -0600

    Augmented Day 12
    During performance discussions, were figuring out fastest ways to do day 12, so updated with a bfs option and timers.

--- src/day12/Day12.kt
+++ src/day12/Day12.kt
@@ -2,6 +2,7 @@ package day12
 
 import readInputString
 import java.lang.Integer.min
+import kotlin.system.measureNanoTime
 
 fun main() {
     fun buildMinMountain(mountain: Array<Array<Int>>, ey: Int, ex: Int): Array<Array<Int>> {
@@ -37,8 +38,52 @@ fun main() {
                 }
             }
         }
-
         return minMountain
+    }
+
+    data class qElement (val y: Int, val x: Int, val dist: Int)
+
+    fun bfs(mountain: Array<Array<Int>>, startLocations: List<qElement>, ey: Int, ex: Int): Int {
+        val queue = mutableListOf<qElement>()
+        val visited = Array(mountain.size) { Array(mountain[0].size ) { false } }
+        for (e in startLocations) {
+            queue.add(qElement(e.y, e.x, 0))
+            visited[e.y][e.x] = true
+        }
+
+        while (queue.isNotEmpty()) {
+            val n = queue.removeFirst()
+            // UP
+            if (n.y > 0 && mountain[n.y-1][n.x] <= mountain[n.y][n.x] + 1 && !visited[n.y-1][n.x]) {
+                if (n.y - 1 == ey && n.x == ex)
+                    return n.dist + 1
+                queue.add(qElement(n.y - 1, n.x, n.dist + 1))
+                visited[n.y-1][n.x] = true
+            }
+            // DOWN
+            if (n.y < mountain.size - 1 && mountain[n.y+1][n.x] <= mountain[n.y][n.x] + 1 && !visited[n.y+1][n.x]) {
+                if (n.y + 1 == ey && n.x == ex)
+                    return n.dist + 1
+                queue.add(qElement(n.y + 1, n.x, n.dist + 1))
+                visited[n.y+1][n.x] = true
+            }
+            // LEFT
+            if (n.x > 0 && mountain[n.y][n.x-1] <= mountain[n.y][n.x] + 1 && !visited[n.y][n.x-1]) {
+                if (n.y == ey && n.x - 1 == ex)
+                    return n.dist + 1
+                queue.add(qElement(n.y, n.x - 1, n.dist + 1))
+                visited[n.y][n.x-1] = true
+            }
+            // RIGHT
+            if (n.x < mountain[0].size - 1 && mountain[n.y][n.x+1] <= mountain[n.y][n.x] + 1 && !visited[n.y][n.x+1]) {
+                if (n.y == ey && n.x + 1 == ex)
+                    return n.dist + 1
+                queue.add(qElement(n.y, n.x + 1, n.dist + 1))
+                visited[n.y][n.x+1] = true
+            }
+        }
+
+        return -1
     }
 
     fun part1(input: List<String>): Int {
@@ -64,7 +109,17 @@ fun main() {
             }
         }
 
-        val minMountain = buildMinMountain(mountain, ey, ex)
+        var minMountain = buildMinMountain(mountain, ey, ex)
+        val minMountainTime = measureNanoTime { minMountain = buildMinMountain(mountain, ey, ex) }
+
+        var bfsAnswer = bfs(mountain, listOf(qElement(sy, sx, 0)), ey, ex)
+        val bfsTime = measureNanoTime { bfsAnswer = bfs(mountain, listOf(qElement(sy, sx, 0)), ey, ex) }
+
+        println("=== PART ONE ===")
+        println("  MinPathMountain Answer = ${minMountain[sy][sx]}")
+        println("  BFS Answer             = $bfsAnswer")
+        println("  MinPathMountain Time   = ${"%,d".format(minMountainTime).padStart(12)} ns")
+        println("  BFS Time               = ${"%,d".format(bfsTime).padStart(12)} ns")
         return minMountain[sy][sx]
     }
 
@@ -72,10 +127,12 @@ fun main() {
         val mountain = Array<Array<Int>>(input.size) { Array(input[0].length) { 0 } }
         var ey = 0
         var ex = 0
+        var starts = mutableListOf<qElement>()
 
         for (y in input.indices) {
             for (x in 0 until input[y].length) {
-                if (input[y][x] == 'S') {
+                if (input[y][x] == 'S' || input[y][x] == 'a') {
+                    starts.add(qElement(y, x, 0))
                     mountain[y][x] = 0
                 } else if (input[y][x] == 'E') {
                     ey = y
@@ -87,17 +144,29 @@ fun main() {
             }
         }
 
-        val minMountain = buildMinMountain(mountain, ey, ex)
-
         var minPath = Int.MAX_VALUE
-        for (y in input.indices) {
-            for (x in 0 until input[y].length) {
-                if (input[y][x] == 'a' || input[y][x] == 'S') {
-                    minPath = min(minPath, minMountain[y][x])
+        val minMountainTime = measureNanoTime {
+            val minMountain = buildMinMountain(mountain, ey, ex)
+
+            for (y in input.indices) {
+                for (x in 0 until input[y].length) {
+                    if (input[y][x] == 'a' || input[y][x] == 'S') {
+                        minPath = min(minPath, minMountain[y][x])
+                    }
                 }
             }
         }
 
+        var bfsAnswer = Int.MAX_VALUE
+        val bfsTime = measureNanoTime {
+            bfsAnswer = bfs(mountain, starts, ey, ex)
+        }
+
+        println("=== PART TWO ===")
+        println("  MinPathMountain Answer = $minPath")
+        println("  BFS Answer             = $bfsAnswer")
+        println("  MinPathMountain Time   = ${"%,d".format(minMountainTime).padStart(12)} ns")
+        println("  BFS Time               = ${"%,d".format(bfsTime).padStart(12)} ns")
         return minPath
     }
 
---
+++ src/day13/Day13.kt
@@ -0,0 +1,26 @@
+package day13
+
+import readInputString
+import java.lang.Integer.min
+import java.util.LinkedList
+import java.util.Queue
+import kotlin.system.measureNanoTime
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val testInput = readInputString("day13/test")
+    val input = readInputString("day13/input")
+
+    check(part1(testInput) == 0)
+    println(part1(input))
+
+    check(part2(testInput) == 0)
+    println(part2(input))
+}
